ddd3ccb1ae056047dc7e7c015be2cc6ac11498d3
burak seçer <bsecer@mimeda.com.tr>
2025-12-24 15:26:05 +0300

    add package swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+import PackageDescription
+
+let package = Package(
+    name: "bidding-mobile-ios-sdk-release",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "bidding_mobile_ios_sdk",
+            targets: ["bidding_mobile_ios_sdk"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "bidding_mobile_ios_sdk",
+            url: "https://github.com/Mimeda/bidding-mobile-ios-sdk-release/releases/download/v1.0.0/bidding_mobile_ios_sdk.xcframework.zip",
+            checksum: "sha256:a6468f4578722bfed4d990f8fe8b6dfd286452fdbf69c6fcb39848fbc25b2762"
+        )
+    ]
+)
+
